18853981a809a7a7082d731a7fe2f7e554d0877f
louiml <99025570+funmmer@users.noreply.github.com>
2022-11-01 16:56:09 +0200

    Create color.hpp

---
+++ includes/color.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+namespace color{
+string black = "\u001b[30m";
+string red = "\u001b[31m";
+string green = "\u001b[32m";
+string yellow = "\u001b[33m";
+string blue = "\u001b[34m";
+string magenta = "\u001b[35m";
+string cyan = "\u001b[36m";
+string white = "\u001b[37m";
+string reset = "\u001b[0m";
+}
